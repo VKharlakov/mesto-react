01923d2fbc2c4b82b8b6432038080b7b4a6614be
Vitaly Kharlakov <vkharlakov@yandex.ru>
2023-03-11 21:06:00 +0100

    ПР10: исправить форматирование, валидацию, попапы, зависимости

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,23 @@
 import React from "react";
 
 function Card(card) {
-    
+
     function handleCardClick() {
         card.onCardClick(card);
-    } 
+    }
 
     return (
-    <li className="elements__element" onClick={handleCardClick}>
-        <button className="elements__delete-button elements__delete-button_hidden" type="button" aria-label="Удалить"/>
-        <img className="elements__photo" src={card.link} alt={card.name} />
-        <div className="elements__info">
-            <h2 className="elements__title">{card.name}</h2>
-            <div className="elements__likes">
-                <button className="elements__like-button" type="button" aria-label="Оценить"/>
-                <p className="elements__like-count">{card.likes}</p>
+        <li className="elements__element">
+            <button className="elements__delete-button elements__delete-button_hidden" type="button" aria-label="Удалить" />
+            <img className="elements__photo" src={card.link} alt={card.name} onClick={handleCardClick} />
+            <div className="elements__info">
+                <h2 className="elements__title">{card.name}</h2>
+                <div className="elements__likes">
+                    <button className="elements__like-button" type="button" aria-label="Оценить" />
+                    <p className="elements__like-count">{card.likes}</p>
+                </div>
             </div>
-        </div>
-    </li>
+        </li>
     )
 }
 
