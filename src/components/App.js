47109c29d3d774d3ce7885677fb13832808e5657
Vitaly Kharlakov <vkharlakov@yandex.ru>
2023-03-11 20:16:12 +0100

    ПР10: перенести проект на реакт

--- src/components/App.js
+++ src/components/App.js
@@ -1,93 +1,82 @@
+import React from 'react';
 import '../App.css';
 import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
+import PopupWithForm from './PopupWithForm';
+import ImagePopup from './ImagePopup';
 
 function App() {
-  return (
-    <body class="page">
-      <Header />
-      <Main />
-      <Footer />
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
+  const [selectedCard, setSelectedCard] = React.useState({});
 
+  function closeAllPopups() {
+    setIsEditProfilePopupOpen(false)
+    setIsAddPlacePopupOpen(false)
+    setIsEditAvatarPopupOpen(false)
+    setSelectedCard({})
+  }
+
+  return (
+    <div className="page">
+      <Header />
+      <Main 
+        onEditProfile={setIsEditProfilePopupOpen}
+        onAddPlace={setIsAddPlacePopupOpen}
+        onEditAvatar={setIsEditAvatarPopupOpen}
+        onCardClick={setSelectedCard}
+      />
+      <Footer />
+      
       {/* <!-- Окно редактирования информации пользователя --> */}
-      <div class="popup popup_type_edit-profile" id="edit-profile">
-        <button class="popup__btn-close" type="button" aria-label="Закрыть"></button>
-        <div class="popup__container">
-          <h2 class="popup__title">Редактировать профиль</h2>
-          <form class="popup__form" id="edit-profile-form" name="form" novalidate>
-            <input class="popup__input popup__input_type_name" id="input-name" placeholder="Введите имя" name="name" minlength="2" maxlength="40" type="text" required />
-            <span id="input-name-error" class="popup__error-hint"></span>
-            <input class="popup__input popup__input_type_brief" id="input-brief"placeholder="Опишите себя" name="brief" minlength="2" maxlength="200" type="text" required />
-            <span id="input-brief-error" class="popup__error-hint"></span>
-            <button class="popup__btn-submit" type="submit" aria-label="Сохранить">Сохранить</button>
-          </form>
-        </div>
-      </div>
+      <PopupWithForm 
+        name="edit-profile"
+        title="Редактировать профиль"
+        isOpen={isEditProfilePopupOpen}
+        onClose={closeAllPopups}>
+        <input className="popup__input popup__input_type_name" id="input-name" placeholder="Введите имя" name="name" minLength="2" maxLength="40" type="text" required />
+        <span id="input-name-error" className="popup__error-hint" />
+        <input className="popup__input popup__input_type_brief" id="input-brief"placeholder="Опишите себя" name="brief" minLength="2" maxLength="200" type="text" required />
+        <span id="input-brief-error" className="popup__error-hint" />
+      </PopupWithForm>
 
       {/* <!-- Окно добавления фотографий --> */}
-      <div class="popup popup_type_add-photos" id="add-photos">
-        <button class="popup__btn-close" type="button" aria-label="Закрыть"></button>
-        <div class="popup__container">
-          <h2 class="popup__title">Новое место</h2>
-          <form class="popup__form" id="add-photos-form" novalidate>
-            <input class="popup__input popup__input_type_photo-title" placeholder="Введите название места" name="name" id="mesto-name" minlength="2" maxlength="30" type="text" required />
-            <span class="popup__error-hint" id="mesto-name-error"></span>
-            <input class="popup__input popup__input_type_photo-link" placeholder="Ссылка на фотографию" name="link" id="mesto-link" type="url" required />
-            <span class="popup__error-hint" id="mesto-link-error"></span>
-            <button class="popup__btn-submit" type="submit" aria-label="Создать">Создать</button>
-          </form>
-        </div>
-      </div>
-
-      {/* <!-- Окно с полноэкранным режимом фотографий --> */}
-      <div class="popup popup_type_fullscreen-photos" id="fullscreen-photos">
-        <button class="popup__btn-close popup__btn-close_type_photo" type="button" aria-label="Закрыть"></button>
-        <div class="popup__fullscreen">
-          <img class="popup__image-fullscreen" src="#" alt="" />
-          <p class="popup__image-subtitle"></p>
-        </div>
-      </div>
-
-      {/* <!-- Окно подтверждения удаления фотографии --> */}
-      <div class="popup popup_type_delete-confirmation" id="delete-confirmation">
-        <button class="popup__btn-close" type="button" aria-label="Закрыть"></button>
-        <div class="popup__container popup__container_type_delete-confirmation">
-          <h2 class="popup__title">Вы точно хотите удалить?</h2>
-          <form class="popup__form" id="delete-confirmation-form" name="form" novalidate>
-            <button class="popup__btn-submit" type="submit" aria-label="Удалить">Да</button>
-          </form>
-        </div>
-      </div>
+      <PopupWithForm 
+        name="add-photos"
+        title="Новое место"
+        isOpen={isAddPlacePopupOpen}
+        onClose={closeAllPopups}>
+        <input className="popup__input popup__input_type_photo-title" placeholder="Введите название места" name="name" id="mesto-name" minLength="2" maxLength="30" type="text" required />
+        <span className="popup__error-hint" id="mesto-name-error" />
+        <input className="popup__input popup__input_type_photo-link" placeholder="Ссылка на фотографию" name="link" id="mesto-link" type="url" required />
+        <span className="popup__error-hint" id="mesto-link-error" />
+      </PopupWithForm>
 
       {/* <!-- Окно редактирования аватара пользователя --> */}
-      <div class="popup popup_type_edit-profile-avatar" id="edit-profile-avatar">
-        <button class="popup__btn-close" type="button" aria-label="Закрыть"></button>
-        <div class="popup__container popup__container_type_edit-avatar">
-          <h2 class="popup__title">Обновить аватар</h2>
-          <form class="popup__form" id="edit-profile-avatar-form" name="form" novalidate>
-            <input class="popup__input popup__input_type_photo-link" placeholder="Ссылка на фотографию" name="avatar" id="avatar-link" type="url" required />
-            <span class="popup__error-hint" id="avatar-link-error"></span>
-            <button class="popup__btn-submit" type="submit" aria-label="Сохранить">Сохранить</button>
-          </form>
-        </div>
-      </div>
+      <PopupWithForm 
+        name="edit-profile-avatar"
+        title="Обновить аватар"
+        isOpen={isEditAvatarPopupOpen}
+        onClose={closeAllPopups}>
+        <input className="popup__input popup__input_type_photo-link" placeholder="Ссылка на фотографию" name="avatar" id="avatar-link" type="url" required />
+        <span className="popup__error-hint" id="avatar-link-error" />
+      </PopupWithForm>
 
-      {/* <!-- Шаблон карточки с фото --> */}
-      <template class="elements__element-template">
-        <li class="elements__element">
-          <button class="elements__delete-button elements__delete-button_hidden" type="button" aria-label="Удалить"></button>
-          <img class="elements__photo" src="#" alt="" />
-          <div class="elements__info">
-            <h2 class="elements__title"></h2>
-            <div class="elements__likes">
-              <button class="elements__like-button" type="button" aria-label="Оценить"></button>
-              <p class="elements__like-count">1</p>
-            </div>
-          </div>
-        </li>
-      </template>
-	  </body>
+      {/* <!-- Окно с полноэкранным режимом фотографий --> */}
+      <ImagePopup 
+        card={selectedCard}
+        onClose={closeAllPopups}
+      />
+
+      {/* <!-- Окно подтверждения удаления фотографии --> */}
+      <PopupWithForm 
+        name="delete-confirmation"
+        title="Вы точно хотите удалить?"
+      />
+      
+	  </div>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,59 @@
 import React from "react";
-import avatar from "../images/profile/avatar.jpg"
+import api from "../utils/api";
+import Card from "./Card";
 
-function Main() {
+function Main(props) {
+    const [userName, setUserName] = React.useState('');
+    const [userDescription, setUserDescription] = React.useState('');
+    const [userAvatar, setUserAvatar] = React.useState('');
+    const [cards, setCards] = React.useState([]);
+
+    React.useEffect(() => {
+        api.getUserInfo()
+        .then((userData) => {
+            setUserName(userData.name)
+            setUserDescription(userData.about)
+            setUserAvatar(userData.avatar)
+        })
+        .catch((err) => console.log(err))
+        
+        api.getInitialCardSet()
+        .then((cardList) => {
+            setCards(cardList.map((card) => ({
+                name: card.name,
+                link: card.link,
+                likes: card.likes,
+                id: card._id
+            })))
+        })
+    })
+
     return (
-    <main class="content">
-        <section class="profile">
-            <div class="profile__avatar-container">
-                <img class="profile__avatar" src={avatar} alt="Ваш аватар" />
-                <button class="profile__edit-avatar-button"onClick={handleEditAvatarClick}></button>
+    <main className="content">
+        <section className="profile">
+            <div className="profile__avatar-container">
+                <img className="profile__avatar" src={userAvatar} alt="Ваш аватар" />
+                <button className="profile__edit-avatar-button" onClick={() => {props.onEditAvatar(true)}}/>
             </div>
-            <div class="profile__info">
-                <h1 class="profile__name">Жак-Ив Кусто</h1>
-                <button class="profile__edit-button" id="btn-show-edit-profile-form" type="button" aria-label="Редактировать профиль" name="EditProfile" onClick={handleEditProfileClick}></button>
-                <p class="profile__brief">Исследователь океана</p>
+            <div className="profile__info">
+                <h1 className="profile__name">{userName}</h1>
+                <button className="profile__edit-button" id="btn-show-edit-profile-form" type="button" aria-label="Редактировать профиль" name="EditProfile" onClick={() => {props.onEditProfile(true)}}/>
+                <p className="profile__brief">{userDescription}</p>
             </div>
-            <button class="profile__add-button" id="btn-show-add-photos-form" type="button" aria-label="Добавить" name="AddPhotos" onClick={handleAddPlaceClick}></button>
+            <button className="profile__add-button" id="btn-show-add-photos-form" type="button" aria-label="Добавить" name="AddPhotos" onClick={() => {props.onAddPlace(true)}}/>
         </section>
-        <section class="elements">
-            <ul class="elements__list"></ul>
+        <section className="elements">
+            <ul className="elements__list">
+                {cards.map((card) => (
+                    <Card
+                        name={card.name}
+                        link={card.link}
+                        likes={card.likes.length}
+                        key={card.id}
+                        onCardClick={props.onCardClick}
+                    />
+                ))}
+            </ul>
         </section>
     </main>
     )
